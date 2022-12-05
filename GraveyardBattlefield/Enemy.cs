c39a519e9be82e55ee65dca33ad01101494c455d
Jason Wang <jw6721@rit.edu>
2022-12-05 17:13:22 -0500

    1

--- GraveyardBattlefield/Enemy.cs
+++ GraveyardBattlefield/Enemy.cs
@@ -52,7 +52,7 @@ namespace GraveyardBattlefield
         {
             bool XequalPlayer = false;
             bool YequalPlayer = false;
-            int zombieSpeed = 2;
+            int zombieSpeed = 1;
             int zombieContactWithPlayer = 10;
             //change X value base on player's X value
             if (player.Position.X + zombieContactWithPlayer < position.X) 
